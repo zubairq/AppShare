e70711cc8009af8ddeea872d54c00c95d7922f62
Zubair Quraishi <zq@nemcv.com>
2014-09-06 21:24:13 +0200

    Added debug profile

--- project.clj
+++ project.clj
@@ -134,6 +134,35 @@
 
                }
               }
+
+
+             :prod_debug
+             {
+              :source-paths ["src" "../srcproddebug"]
+              :cljsbuild
+              {
+               :builds
+               [
+                {
+                 :source-paths ["src"]
+                 :compiler     {
+                                :output-to      "resources/public/main.js"
+                                :optimizations  :advanced
+                                :output-wrapper false
+                                :externs        ["resources/public/jquery-externs.js"
+                                                 "resources/public/google_maps_api_v3_11.js"
+                                                 "resources/public/reactextern.js"]
+                                :pretty-print   false
+                                :foreign-libs [{:file "https://maps.googleapis.com/maps/api/js?sensor=false"
+                                                :provides  ["google.maps" "google.maps.MapTypeId"]}]
+                                }
+                 }
+                ]
+
+               }
+              }
+
+
              }
 
 
